1f72f68e3617f6a61e6d79a98b66b7dce7d9861d
Student <Sandmen461@gmail.com>
2021-02-22 20:31:51 +0200

    Initial commit

---
+++ app/src/main/java/com/example/mycalendary/MainActivity.java
@@ -0,0 +1,92 @@
+package com.example.mycalendary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.app.DatePickerDialog;
+import android.content.ActivityNotFoundException;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.text.format.DateUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int REQUEST_TAKE_PHOTO = 1;
+    private ImageView imageView;
+    Button btnDate;
+    TextView txtDate;
+    Calendar dateAndTime=Calendar.getInstance();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       // imageView = (ImageView)findViewById(R.id.imageView);
+
+        btnDate=findViewById(R.id.button2);
+
+       /// setInitialDate();
+    }
+
+
+    public void setDate(View v){
+        new DatePickerDialog(MainActivity.this,
+                d,
+                dateAndTime.get(Calendar.YEAR),
+                dateAndTime.get(Calendar.MONTH),
+                dateAndTime.get(Calendar.DAY_OF_MONTH)).show();
+                //this .setPositiveButton("OK", null);
+    }
+
+    DatePickerDialog.OnDateSetListener d= new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+            dateAndTime.set(Calendar.YEAR,year);
+            dateAndTime.set(Calendar.MONTH,month);
+            dateAndTime.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+
+            //setInitialDate();
+        }
+    };
+//    private void setInitialDate(){
+//        txtDate.setText(DateUtils.formatDateTime(this,dateAndTime.getTimeInMillis(),DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR));
+//    }
+    public void onClick(View v){
+        Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(takePhotoIntent, REQUEST_TAKE_PHOTO);
+        }catch (ActivityNotFoundException ex){
+            ex.printStackTrace();
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK){
+            Bundle bundle = data.getExtras();
+            Bitmap bitmap = (Bitmap)bundle.get("data");
+
+        }
+    }
+}
